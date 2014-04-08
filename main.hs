92b2e0f3c784d9c7d7a46ce6be0f44fe20340274
Anish Tondwalkar <tondwalkar@virginia.edu>
2014-04-08 16:56:27 -0400

    Parsing.

--- main.hs
+++ main.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+import Control.Applicative ((<$>),(<*>))
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -8,7 +10,60 @@ symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~#"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left e -> "Parse Error: " ++ show e
     Right _ -> "You Parse!"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do 
+ char '"'
+ x <- many (noneOf "\"")
+ char '"'
+ return$ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  return$ case first:rest of 
+	  "#t" -> Bool True
+	  "#f" -> Bool False
+	  _ -> Atom$ first:rest
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseList :: Parser LispVal
+parseList = List <$> sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  h <- endBy parseExpr spaces
+  t <- char '.' >> spaces >> parseExpr
+  return$ DottedList h t
+
+parseQuoted :: Parser LispVal
+parseQuoted =
+  char '\'' >>
+  parseExpr >>=
+  return . List . ((Atom "quote"):) . (:[])
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+         <|> parseQuoted
+         <|> do char '('
+                x <- (try parseList) <|> parseDottedList
+                char ')'
+                return x
